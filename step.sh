595fcdf2c87c4b8eac56961beb865517859bbde0
Tommaso Madonia <tommaso@madonia.me>
2016-05-08 19:54:09 +0200

    First commit

---
+++ step.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
+source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
+
+
+if [ -z "${source_root_path}" ]; then
+  write_section_to_formatted_output "# Error"
+  write_section_start_to_formatted_output '* source_root_path input is missing'
+  exit 1
+fi
+print_and_do_command_exit_on_error cd "${source_root_path}"
+
+write_section_start_to_formatted_output "# Installing CocoaSeeds"
+print_and_do_command_exit_on_error gem install cocoaseeds
+write_section_start_to_formatted_output "## Current CocoaSeeds version"
+seed_version=$(seed --version)
+if [ $? -ne 0 ]; then
+	write_section_to_formatted_output "# Error"
+	write_section_start_to_formatted_output '* Failed to get current CocoaSeeds version'
+	exit 1
+fi
+write_section_start_to_formatted_output "    ${seed_version}"
+
+write_section_to_formatted_output "# Run seed install"
+print_and_do_command_exit_on_error seed install
+
+exit 0
